df48a5fe8711ac02b0470f87acb6f8e0eda620ea
Gyuwon-Jung <licmm0520@gmail.com>
2024-04-02 17:24:13 +0900

    AudioCrad component

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import AudioCard from '../components/contents/Audiocard'
 
 const Main = () => {
   return (
-    <div>Main</div>
+    <div><AudioCard 
+    imageUrl="https://nerd-lee.github.io/assets/img/etc/styledcomponents.png"
+    category="라이프스타일"
+    title="한국의 벚꽃이 일본의 국화가 된 이유"
+    writer="관계자외 출입 금지"
+    tag="#벚꽃"
+  /></div>
   )
 }
 
