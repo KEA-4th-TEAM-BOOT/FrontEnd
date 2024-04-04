db9dbece6e2bcef443217143ad020ff500657af8
Gyuwon-Jung <licmm0520@gmail.com>
2024-04-04 10:47:52 +0900

    header complete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,11 @@ const Home = () => {
             title = "Auda : 글이 내게로 오다"
             description="내 목소리로 들려주는 글 : Auda">
             
-            <Banner />
+            <Banner
+            imageUrl= "https://t1.kakaocdn.net/friends/prod/main_tab/home/home_20201106164745_kr.jpg?type=thumb&opt=R329x247@2xa"
+            contentTitle= "카카오"
+            contentWriter= "Suzuki"
+              />
             <Feed />
             <Trend />
             <Footer />
