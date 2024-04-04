48c4b4d189a94711682742f0e1421d450fde28ba
7SongMinJi <starr075@naver.com>
2024-04-04 17:11:59 +0900

    Recommend 임시

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,75 +1,70 @@
-import React from 'react'
-import Main from './Main'
-import Banner from '../components/contents/Banner'
-import Feed from '../components/contents/Feed'
-import Trend from '../components/contents/Trend'
-import Footer from './Footer'
+import React from "react";
+import Main from "./Main";
+import Banner from "../components/contents/Banner";
+import Feed from "../components/contents/Feed";
+import Recommend from "../components/contents/Recommend";
+import Footer from "./Footer";
 
-import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
 
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
-
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
 const Home = () => {
-    return (
-        <Main
-            title="Auda : 글이 내게로 오다"
-            description="내 목소리로 들려주는 글 : Auda">
+  return (
+    <Main
+      title="Auda : 글이 내게로 오다"
+      description="내 목소리로 들려주는 글 : Auda"
+    >
+      <Swiper
+        modules={[Navigation, Pagination]}
+        spaceBetween={10}
+        slidesPerView={1}
+        autoplay={{ delay: 500, disableOnInteraction: false }}
+        loop={true}
+      >
+        <SwiperSlide>
+          <Banner
+            imageUrl="https://t1.kakaocdn.net/friends/prod/main_tab/home/home_20201106164745_kr.jpg?type=thumb&opt=R329x247@2xa"
+            contentTitle="카카오"
+            contentWriter="Suzuki"
+          />
+        </SwiperSlide>
 
-            <Swiper
-                modules={[Navigation, Pagination]}
-                spaceBetween={10}
-                slidesPerView={1}
-                autoplay={{ delay: 500, disableOnInteraction: false }}
-                loop={true}
-            >
-                <SwiperSlide>
-                    <Banner
-                        imageUrl="https://t1.kakaocdn.net/friends/prod/main_tab/home/home_20201106164745_kr.jpg?type=thumb&opt=R329x247@2xa"
-                        contentTitle="카카오"
-                        contentWriter="Suzuki"
-                    />
-                </SwiperSlide>
+        <SwiperSlide>
+          <Banner
+            imageUrl="https://t1.kakaocdn.net/friends/prod/main_tab/home/home_20201106164745_kr.jpg?type=thumb&opt=R329x247@2xa"
+            contentTitle="카카오1"
+            contentWriter="Suzuki1"
+          />
+        </SwiperSlide>
 
-                <SwiperSlide>
-                    <Banner
-                        imageUrl="https://t1.kakaocdn.net/friends/prod/main_tab/home/home_20201106164745_kr.jpg?type=thumb&opt=R329x247@2xa"
-                        contentTitle="카카오1"
-                        contentWriter="Suzuki1"
-                    />
-                </SwiperSlide>
+        <SwiperSlide>
+          <Banner
+            imageUrl="https://t1.kakaocdn.net/friends/prod/main_tab/home/home_20201106164745_kr.jpg?type=thumb&opt=R329x247@2xa"
+            contentTitle="카카오2"
+            contentWriter="Suzuki2"
+          />
+        </SwiperSlide>
 
-                <SwiperSlide>
-                    <Banner
-                        imageUrl="https://t1.kakaocdn.net/friends/prod/main_tab/home/home_20201106164745_kr.jpg?type=thumb&opt=R329x247@2xa"
-                        contentTitle="카카오2"
-                        contentWriter="Suzuki2"
-                    />
-                </SwiperSlide>
+        <SwiperSlide>
+          <Banner
+            imageUrl="https://t1.kakaocdn.net/friends/prod/main_tab/home/home_20201106164745_kr.jpg?type=thumb&opt=R329x247@2xa"
+            contentTitle="카카오3"
+            contentWriter="Suzuki3"
+          />
+        </SwiperSlide>
+      </Swiper>
 
-                <SwiperSlide>
-                    <Banner
-                        imageUrl="https://t1.kakaocdn.net/friends/prod/main_tab/home/home_20201106164745_kr.jpg?type=thumb&opt=R329x247@2xa"
-                        contentTitle="카카오3"
-                        contentWriter="Suzuki3"
-                    />
-                </SwiperSlide>
+      <Feed />
+      <Recommend />
+      <Footer />
+    </Main>
+  );
+};
 
-            </Swiper>
-
-
-
-            <Feed />
-            <Trend />
-            <Footer />
-
-        </Main>
-    )
-}
-
-export default Home
+export default Home;
