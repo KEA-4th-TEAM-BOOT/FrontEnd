0af8bce91df4b705a279e720ea5b534823c599e3
Gyuwon-Jung <licmm0520@gmail.com>
2024-04-11 11:35:41 +0900

    Banner 수정

--- src/components/homeSection/Banner.jsx
+++ src/components/homeSection/Banner.jsx
@@ -4,23 +4,32 @@ import styled from "styled-components";
 import { Swiper, SwiperSlide } from "swiper/react";
 import 'swiper/css';
 import 'swiper/css/navigation';
-import { Navigation} from 'swiper/modules';
+import { Navigation } from 'swiper/modules';
 
 const Banner = () => {
   return (
     <BannerWrapper>
       <Swiper navigation={true} modules={[Navigation]} className="mySwiper"
-      slidesPerView={1} // 한 번에 보여줄 슬라이드 수
+        slidesPerView={1} // 한 번에 보여줄 슬라이드 수
       >
-        <StyledSwiperSlide>Slide 1</StyledSwiperSlide>
-        <StyledSwiperSlide>Slide 2</StyledSwiperSlide>
-        <StyledSwiperSlide>Slide 3</StyledSwiperSlide>
-        <StyledSwiperSlide>Slide 4</StyledSwiperSlide>
-        <StyledSwiperSlide>Slide 5</StyledSwiperSlide>
-        <StyledSwiperSlide>Slide 6</StyledSwiperSlide>
-        <StyledSwiperSlide>Slide 7</StyledSwiperSlide>
-        <StyledSwiperSlide>Slide 8</StyledSwiperSlide>
-        <StyledSwiperSlide>Slide 9</StyledSwiperSlide>
+        <StyledSwiperSlide>
+          <BannerContent bgcolor="#C5D9FF">
+            <BannerHeding>오디오 블로그</BannerHeding>
+          </BannerContent>
+        </StyledSwiperSlide>
+
+        <StyledSwiperSlide>
+        <BannerContent bgcolor="#DAFFF4">
+          2
+          </BannerContent>
+        </StyledSwiperSlide>
+
+        <StyledSwiperSlide>
+        <BannerContent bgcolor="#FAC5FF">
+        3
+          </BannerContent>
+        </StyledSwiperSlide>
+
       </Swiper>
     </BannerWrapper>
   );
@@ -38,12 +47,37 @@ const BannerWrapper = styled.div`
 `;
 
 const StyledSwiperSlide = styled(SwiperSlide)`
-  display: flex;
+  display: table;
   width: auto;
+  position: relative;
   justify-content: center;
   align-items: center;
   font-size: 20px; // 글자 크기 조정
 `;
+
+const BannerContent = styled.div`
+  width:100%;
+  height:100%;
+  position:relative;
+  background-color: ${(props) => props.bgcolor};
+  display: table-cell;
+  padding: 40px 0 0;
+  vertical-align: middle;
+`;
+
+const BannerHeding = styled.strong`
+  position: relative;
+  width: 1080px;
+  margin: 0 auto;
+  text-align: left;
+`
+
+const BannerDescription = styled.p`
+  font-size: 16px;
+  line-height: 26px;
+  color: rgba(0, 0, 0, .8);
+  letter-spacing: -.2px;
+`
 
 // 추가적으로 .mySwiper 클래스를 사용하여 CSS를 정의할 수 있음
 // 예: .mySwiper .swiper-slide { 스타일 규칙 }
