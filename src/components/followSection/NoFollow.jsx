350eac9bcacfd4a2d46f4ea3a8d8498370f310f2
7SongMinJi <starr075@naver.com>
2024-04-19 10:09:22 +0900

    조금 수정

--- src/components/followSection/NoFollow.jsx
+++ src/components/followSection/NoFollow.jsx
@@ -6,42 +6,42 @@ const NoFollow = () => {
   const followersData = [
     {
       profileImageUrl:
-        "https://lh5.googleusercontent.com/p/AF1QipN-CN6qlIZYm3sgIfj7QNumuWcbjLb_FMwY4n8u=w360-h240-n-k-no",
+        "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcR9w38ygfN8g_N3j9HNxWvKfI09e1nBF0nJBnrOMjFdzuTdEgLb",
       nickname: "취미도서관",
       followerCount: 32,
       description: "블로그를 취미로 하는 명예 사서",
     },
     {
       profileImageUrl:
-        "https://lh5.googleusercontent.com/p/AF1QipN-CN6qlIZYm3sgIfj7QNumuWcbjLb_FMwY4n8u=w360-h240-n-k-no",
+        "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRAYHTg2J5bnKDwTG3eBF4UwmqFwrnfWy2ZUO7U12fUHlPjhrMd",
       nickname: "취미도서관",
       followerCount: 32,
       description: "블로그를 취미로 하는 명예 사서",
     },
     {
       profileImageUrl:
-        "https://lh5.googleusercontent.com/p/AF1QipN-CN6qlIZYm3sgIfj7QNumuWcbjLb_FMwY4n8u=w360-h240-n-k-no",
+        "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRMbUQsR62KO6NO65uLrid-12zDjzO8dgilJbpr7kVyj1c6WQTi",
       nickname: "취미도서관",
       followerCount: 32,
       description: "블로그를 취미로 하는 명예 사서",
     },
     {
       profileImageUrl:
-        "https://lh5.googleusercontent.com/p/AF1QipN-CN6qlIZYm3sgIfj7QNumuWcbjLb_FMwY4n8u=w360-h240-n-k-no",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0K7M81E15mDZubxH6HlcCgZKN6lig6v26n6K8JLTEg9baeaKJ",
       nickname: "취미도서관",
       followerCount: 32,
       description: "블로그를 취미로 하는 명예 사서",
     },
     {
       profileImageUrl:
-        "https://lh5.googleusercontent.com/p/AF1QipN-CN6qlIZYm3sgIfj7QNumuWcbjLb_FMwY4n8u=w360-h240-n-k-no",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIapPvmsEr5XbJPL8ZM7GDX2z2IN1gSH-Chi8kKcCxXDl8Wx74",
       nickname: "취미도서관",
       followerCount: 32,
       description: "블로그를 취미로 하는 명예 사서",
     },
     {
       profileImageUrl:
-        "https://lh5.googleusercontent.com/p/AF1QipN-CN6qlIZYm3sgIfj7QNumuWcbjLb_FMwY4n8u=w360-h240-n-k-no",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyR1OuTgfZhRlsZ328_F_9ymblHv0Qp35Jl3-5Z1tai6SOlKHc",
       nickname: "취미도서관",
       followerCount: 32,
       description: "블로그를 취미로 하는 명예 사서",
@@ -53,18 +53,20 @@ const NoFollow = () => {
         <Title>팔로잉 중인 블로그가 없어요</Title>
         <Subtitle>다양한 블로그를 팔로우 해보세요</Subtitle>
       </ContentSection>
-      <RecommendationSection>
-        <SectionTitle>팔로잉 추천</SectionTitle>
-        <CardGrid>
-          {followersData.length ? (
-            followersData.map((data, index) => (
-              <FollowCard key={index} {...data} />
-            ))
-          ) : (
-            <NoFollowMessage>팔로우할 사용자가 없어요</NoFollowMessage>
-          )}
-        </CardGrid>
-      </RecommendationSection>
+      <SectionBackground>
+        <RecommendationSection>
+          <SectionTitle>팔로잉 추천</SectionTitle>
+          <CardGrid>
+            {followersData.length ? (
+              followersData.map((data, index) => (
+                <FollowCard key={index} {...data} />
+              ))
+            ) : (
+              <NoFollowMessage>팔로우할 사용자가 없어요</NoFollowMessage>
+            )}
+          </CardGrid>
+        </RecommendationSection>
+      </SectionBackground>
     </PageContainer>
   );
 };
@@ -73,12 +75,10 @@ export default NoFollow;
 
 const PageContainer = styled.div`
   width: 100%;
-  max-width: 1200px;
   margin: 0 auto;
   padding: 20px;
   display: flex;
   flex-direction: column;
-  align-items: center;
 `;
 
 const ContentSection = styled.section`
@@ -86,30 +86,43 @@ const ContentSection = styled.section`
 `;
 
 const Title = styled.h2`
-  font-size: 40px;
-  font-weight: bold;
-  color: #333;
-  margin: 50px 0;
+  margin-top: 104px;
+  color: #000;
+  margin-bottom: 50px;
+  margin-left: 200px;
+  font-size: 60px;
+  font-weight: 800;
   text-align: left;
+  width: 100%;
 `;
 
 const Subtitle = styled.p`
   font-size: 20px;
-  color: #666;
+  color: #333;
   text-align: left;
   margin-bottom: 50px;
+  margin-left: 250px;
+  font-weight: bold;
+`;
+
+const SectionBackground = styled.section`
+  background-color: rgba(0, 150, 255, 0.2);
+  padding: 20px;
+  width: 100%;
+  align-self: center;
 `;
 
 const RecommendationSection = styled.section`
-  background-color: #f7f7f7;
-  padding: 20px;
+  padding: 30px;
+  max-width: 900px;
+  margin: 0 auto;
   width: 100%;
 `;
 
 const SectionTitle = styled.h3`
-  font-size: 25px;
-  color: #333;
-  margin-bottom: 20px;
+  font-size: 40px;
+  color: #111;
+  margin-bottom: 50px;
   text-align: left;
 `;
 
@@ -117,8 +130,8 @@ const CardGrid = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   grid-gap: 25px;
-  width: 100%;
-  padding-bottom: 50px;
+  max-width: 1200px;
+  margin: 30px auto;
 `;
 
 const NoFollowMessage = styled.p`
