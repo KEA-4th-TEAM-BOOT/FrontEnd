92f22a936e6a2d1e4cdf3a59286b865e0aae83be
Gyuwon-Jung <licmm0520@gmail.com>
2024-06-05 15:02:23 +0900

    profileImage 변경시 s3 업로드 후 적용

--- src/pages/Setting.jsx
+++ src/pages/Setting.jsx
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
 import EditSetting from "../components/settingSection/EditSetting";
-
 import { fetchUser } from "../api/UserAPI";
 import { UserProfileState } from "../recoil/user";
 import { useSetRecoilState } from "recoil";
@@ -8,34 +7,35 @@ import { useSetRecoilState } from "recoil";
 const Setting = () => {
   const setUserProfileState = useSetRecoilState(UserProfileState);
 
-  useEffect(() => {
-    const fetchUserInfo = async () => {
-      try {
-        const userInfo = await fetchUser();
-        setUserProfileState({
-          name: userInfo.name,
-          email: userInfo.email,
-          nickname: userInfo.nickname,
-          profileUrl: userInfo.profileUrl,
-          introduce: userInfo.introduce,
-          userLink: userInfo.userLink,
-          followingNum: userInfo.followingNum,
-          followerNum: userInfo.followerNum,
-          latestPostId: userInfo.latestPostId,
-          postCnt: userInfo.postCnt,
-          voiceModelUrl: userInfo.voiceModelUrl,
-          categoryList: userInfo.categoryList,
-          followingList: userInfo.followingList,
-          followerList: userInfo.followerList,
-        });
-        console.log(userInfo);
-      } catch (error) {
-        console.error("Failed to fetch user info:", error);
-      }
-    };
+  const fetchUserInfo = async () => {
+    try {
+      const userInfo = await fetchUser();
+      setUserProfileState({
+        name: userInfo.name,
+        email: userInfo.email,
+        nickname: userInfo.nickname,
+        profileUrl: userInfo.profileUrl,
+        introduce: userInfo.introduce,
+        userLink: userInfo.userLink,
+        followingNum: userInfo.followingNum,
+        followerNum: userInfo.followerNum,
+        latestPostId: userInfo.latestPostId,
+        postCnt: userInfo.postCnt,
+        voiceModelUrl: userInfo.voiceModelUrl,
+        categoryList: userInfo.categoryList,
+        followingList: userInfo.followingList,
+        followerList: userInfo.followerList,
+      });
+      console.log(userInfo);
+    } catch (error) {
+      console.error("Failed to fetch user info:", error);
+    }
+  };
 
+  useEffect(() => {
     fetchUserInfo();
-  }, []);
+  }, [setUserProfileState]);
+
   return (
     <>
       <EditSetting />
