b0dfa26b772a41c67ddc8498f8250f5a02a36efe
7SongMinJi <starr075@naver.com>
2024-06-08 16:12:48 +0900

    Mypage 글 목록 pagination

--- src/components/mypageSection/Postlist.jsx
+++ src/components/mypageSection/Postlist.jsx
@@ -1,107 +1,14 @@
+import React from "react";
 import ListCard from "../card/Listcard";
 
-const Postlist = ({ postList, userLink }) => {
-  const postData = [
-    {
-      title: "고양이",
-      content:
-        "고양이(Felis catus)는 포유류 동물이다. 고양이의 신체적 특성과 습성은 다른 고양이과 동물들과 동일하여 빠른 반사신경, 탁월한 유연성, 날카로운 이빨, 넣고 꺼낼 수 있는 발톱 등이 있다.",
-      tags: [
-        "# 고양이1",
-        "# 나만없어고양이",
-        "# 꽁꽁얼어붙은한강위로고양이가걸어다닙니다",
-      ],
-      date: "2024.03.19",
-      likes: 10,
-      comments: 5,
-      thumbnail:
-        "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSRJntz_XPFv_S8YYjqI4N_DCgIPdrO47NIOTwPK1qFY-p-Enhh",
-      audioSrc: "https://youtu.be/sRmbrE6eHpU?si=4ywvHI8tAc049nkZ",
-    },
-    {
-      title: "고양이",
-      content:
-        "고양이(Felis catus)는 포유류 동물이다. 고양이의 신체적 특성과 습성은 다른 고양이과 동물들과 동일하여 빠른 반사신경, 탁월한 유연성, 날카로운 이빨, 넣고 꺼낼 수 있는 발톱 등이 있다.",
-      tags: [
-        "# 고양이1",
-        "# 나만없어고양이",
-        "# 꽁꽁얼어붙은한강위로고양이가걸어다닙니다",
-      ],
-      date: "2024.03.19",
-      likes: 10,
-      comments: 5,
-      thumbnail:
-        "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSRJntz_XPFv_S8YYjqI4N_DCgIPdrO47NIOTwPK1qFY-p-Enhh",
-      audioSrc: "https://youtu.be/sRmbrE6eHpU?si=4ywvHI8tAc049nkZ",
-    },
-    {
-      title: "고양이",
-      content:
-        "고양이(Felis catus)는 포유류 동물이다. 고양이의 신체적 특성과 습성은 다른 고양이과 동물들과 동일하여 빠른 반사신경, 탁월한 유연성, 날카로운 이빨, 넣고 꺼낼 수 있는 발톱 등이 있다.",
-      tags: [
-        "# 고양이1",
-        "# 나만없어고양이",
-        "# 꽁꽁얼어붙은한강위로고양이가걸어다닙니다",
-      ],
-      date: "2024.03.19",
-      likes: 10,
-      comments: 5,
-      thumbnail:
-        "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSRJntz_XPFv_S8YYjqI4N_DCgIPdrO47NIOTwPK1qFY-p-Enhh",
-      audioSrc: "https://youtu.be/sRmbrE6eHpU?si=4ywvHI8tAc049nkZ",
-    },
-    {
-      title: "고양이",
-      content:
-        "고양이(Felis catus)는 포유류 동물이다. 고양이의 신체적 특성과 습성은 다른 고양이과 동물들과 동일하여 빠른 반사신경, 탁월한 유연성, 날카로운 이빨, 넣고 꺼낼 수 있는 발톱 등이 있다.",
-      tags: [
-        "# 고양이1",
-        "# 나만없어고양이",
-        "# 꽁꽁얼어붙은한강위로고양이가걸어다닙니다",
-      ],
-      date: "2024.03.19",
-      likes: 10,
-      comments: 5,
-      thumbnail:
-        "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSRJntz_XPFv_S8YYjqI4N_DCgIPdrO47NIOTwPK1qFY-p-Enhh",
-      audioSrc: "https://youtu.be/sRmbrE6eHpU?si=4ywvHI8tAc049nkZ",
-    },
-    {
-      title: "고양이",
-      content:
-        "고양이(Felis catus)는 포유류 동물이다. 고양이의 신체적 특성과 습성은 다른 고양이과 동물들과 동일하여 빠른 반사신경, 탁월한 유연성, 날카로운 이빨, 넣고 꺼낼 수 있는 발톱 등이 있다.",
-      tags: [
-        "# 고양이1",
-        "# 나만없어고양이",
-        "# 꽁꽁얼어붙은한강위로고양이가걸어다닙니다",
-      ],
-      date: "2024.03.19",
-      likes: 10,
-      comments: 5,
-      thumbnail:
-        "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSRJntz_XPFv_S8YYjqI4N_DCgIPdrO47NIOTwPK1qFY-p-Enhh",
-      audioSrc: "https://youtu.be/sRmbrE6eHpU?si=4ywvHI8tAc049nkZ",
-    },
-    {
-      title: "고양이",
-      content:
-        "고양이(Felis catus)는 포유류 동물이다. 고양이의 신체적 특성과 습성은 다른 고양이과 동물들과 동일하여 빠른 반사신경, 탁월한 유연성, 날카로운 이빨, 넣고 꺼낼 수 있는 발톱 등이 있다.",
-      tags: [
-        "# 고양이1",
-        "# 나만없어고양이",
-        "# 꽁꽁얼어붙은한강위로고양이가걸어다닙니다",
-      ],
-      date: "2024.03.19",
-      likes: 10,
-      comments: 5,
-      thumbnail:
-        "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSRJntz_XPFv_S8YYjqI4N_DCgIPdrO47NIOTwPK1qFY-p-Enhh",
-      audioSrc: "https://youtu.be/sRmbrE6eHpU?si=4ywvHI8tAc049nkZ",
-    },
-  ];
+const Postlist = ({ postList, userLink, currentPage, itemsPerPage }) => {
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const displayedPosts = postList.slice(startIndex, endIndex);
+
   return (
     <div>
-      {postList.map((post, index) => (
+      {displayedPosts.map((post, index) => (
         <ListCard
           key={index}
           title={post.title}
